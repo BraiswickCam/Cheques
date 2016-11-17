757d09da596ca23ee9199cf1479cbe92908fc7ba
Alex <alexbass666@gmail.com>
2016-11-17 14:10:20 +0000

    Move conversions to constructor

--- Cheques/BankList.cs
+++ Cheques/BankList.cs
@@ -9,7 +9,61 @@ namespace Cheques
     {
         public BankList(string[] values)
         {
-            //Move conversions to constructor
+            this.JobNo = values[0];
+
+            try { this.Booking = Convert.ToInt32(values[1]); }
+            catch (System.FormatException) { throw new FormatException("Booking is not a valid integer"); }
+
+            try { this.Collection = Convert.ToInt32(values[2]); }
+            catch (System.FormatException) { throw new FormatException("Collection is not a valid integer"); }
+
+            this.School = values[3];
+            this.PackType = values[4];
+
+            try { this.CashChequeTotal = Convert.ToDouble(values[5]); }
+            catch (System.FormatException) { throw new FormatException("CashChequeTotal is not a valid double"); }
+
+            try { this.Discount = Convert.ToDouble(values[6]); }
+            catch (System.FormatException) { throw new FormatException("Discount is not a valid double"); }
+
+            try { this.Notes50 = Convert.ToDouble(values[7]); }
+            catch (System.FormatException) { throw new FormatException("Notes50 is not a valid double"); }
+
+            try { this.Notes20 = Convert.ToDouble(values[8]); }
+            catch (System.FormatException) { throw new FormatException("Notes20 is not a valid double"); }
+
+            try { this.Notes10 = Convert.ToDouble(values[9]); }
+            catch (System.FormatException) { throw new FormatException("Notes10 is not a valid double"); }
+
+            try { this.Notes5 = Convert.ToDouble(values[10]); }
+            catch (System.FormatException) { throw new FormatException("Notes5 is not a valid double"); }
+
+            try { this.Coins2 = Convert.ToDouble(values[11]); }
+            catch (System.FormatException) { throw new FormatException("Coins2 is not a valid double"); }
+
+            try { this.Coins1 = Convert.ToDouble(values[12]); }
+            catch (System.FormatException) { throw new FormatException("Coins1 is not a valid double"); }
+
+            try { this.Coins50 = Convert.ToDouble(values[13]); }
+            catch (System.FormatException) { throw new FormatException("Coins50 is not a valid double"); }
+
+            try { this.Coins20 = Convert.ToDouble(values[14]); }
+            catch (System.FormatException) { throw new FormatException("Coins20 is not a valid double"); }
+
+            try { this.Coins10 = Convert.ToDouble(values[15]); }
+            catch (System.FormatException) { throw new FormatException("Coins10 is not a valid double"); }
+
+            try { this.Coins5 = Convert.ToDouble(values[16]); }
+            catch (System.FormatException) { throw new FormatException("Coins5 is not a valid double"); }
+
+            try { this.CoinsBronze = Convert.ToDouble(values[17]); }
+            catch (System.FormatException) { throw new FormatException("CoinsBronze is not a valid double"); }
+
+            try { this.ChequeTotal = Convert.ToDouble(values[18]); }
+            catch (System.FormatException) { throw new FormatException("ChequeTotal is not a valid double"); }
+
+            try { this.VisaTotal = Convert.ToDouble(values[19]); }
+            catch (System.FormatException) { throw new FormatException("VisaTotal is not a valid double"); }
         }
 
         //Variables
