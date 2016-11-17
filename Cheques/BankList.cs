ebfc9dfffcc7d9e0dee556a64292ec9178013f1d
Alex <alexbass666@gmail.com>
2016-11-17 13:59:28 +0000

    Remove conversions from properties and make private set

--- Cheques/BankList.cs
+++ Cheques/BankList.cs
@@ -7,6 +7,11 @@ namespace Cheques
 {
     public class BankList
     {
+        public BankList(string[] values)
+        {
+            //Move conversions to constructor
+        }
+
         //Variables
         private string jobNo, school, packType;
         private int booking, collection;
@@ -14,165 +19,29 @@ namespace Cheques
 
         //Properties
         //Strings
-        public string JobNo { get { return jobNo; } set { jobNo = value; } }
-        public string School { get { return school; } set { school = value; } }
-        public string PackType { get { return packType; } set { packType = value; } }
+        public string JobNo { get { return jobNo; } private set { jobNo = value.ToString(); } }
+        public string School { get { return school; } private set { school = value; } }
+        public string PackType { get { return packType; } private set { packType = value; } }
         
         //Integers
-        public int Booking
-        {
-            get { return booking; }
-            set
-            {
-                try { booking = Convert.ToInt32(value); }
-                catch (System.FormatException) { throw new FormatException("Booking number must be an integer"); }
-            }
-        }
-        public int Collection
-        {
-            get { return collection; }
-            set
-            {
-                try { collection = Convert.ToInt32(value); }
-                catch (System.FormatException) { throw new FormatException("Collection number must be an integer"); }
-            }
-        }
+        public int Booking { get { return booking; } private set { booking = value; } }
+        public int Collection { get { return collection; } private set { collection = value; } }
 
         //Doubles (money)
-        public double Notes50
-        {
-            get { return notes50; }
-            set
-            {
-                try { notes50 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("£50 notes must be a double (decimal number)"); }
-            }
-        }
-        public double Notes20
-        {
-            get { return notes20; }
-            set
-            {
-                try { notes20 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("£20 notes must be a double (decimal number)"); }
-            }
-        }
-        public double Notes10
-        {
-            get { return notes10; }
-            set
-            {
-                try { notes10 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("£10 notes must be a double (decimal number)"); }
-            }
-        }
-        public double Notes5
-        {
-            get { return notes5; }
-            set
-            {
-                try { notes5 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("£5 notes must be a double (decimal number)"); }
-            }
-        }
-        public double Coins2
-        {
-            get { return coins2; }
-            set
-            {
-                try { coins2 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("£2 coins must be a double (decimal number)"); }
-            }
-        }
-        public double Coins1
-        {
-            get { return coins1; }
-            set
-            {
-                try { coins1 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("£1 coins must be a double (decimal number)"); }
-            }
-        }
-        public double Coins50
-        {
-            get { return coins50; }
-            set
-            {
-                try { coins50 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("50p coins must be a double (decimal number)"); }
-            }
-        }
-        public double Coins20
-        {
-            get { return coins20; }
-            set
-            {
-                try { coins20 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("20p coins must be a double (decimal number)"); }
-            }
-        }
-        public double Coins10
-        {
-            get { return coins10; }
-            set
-            {
-                try { coins10 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("10p coins must be a double (decimal number)"); }
-            }
-        }
-        public double Coins5
-        {
-            get { return coins5; }
-            set
-            {
-                try { coins5 = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("5p coins must be a double (decimal number)"); }
-            }
-        }
-        public double CoinsBronze
-        {
-            get { return coinsBronze; }
-            set
-            {
-                try { coinsBronze = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("Bronze coins must be a double (decimal number)"); }
-            }
-        }
-        public double ChequeTotal
-        {
-            get { return chequeTotal; }
-            set
-            {
-                try { chequeTotal = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("Cheque Total must be a double (decimal number)"); }
-            }
-        }
-        public double VisaTotal
-        {
-            get { return visaTotal; }
-            set
-            {
-                try { visaTotal = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("Visa Total must be a double (decimal number)"); }
-            }
-        }
-        public double CashChequeTotal
-        {
-            get { return cashChequeTotal; }
-            set
-            {
-                try { cashChequeTotal = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("Cash and Cheque Total must be a double (decimal number)"); }
-            }
-        }
-        public double Discount
-        {
-            get { return discount; }
-            set
-            {
-                try { discount = Convert.ToDouble(value); }
-                catch (System.FormatException) { throw new FormatException("Discount must be a double (decimal number)"); }
-            }
-        }
+        public double Notes50 { get { return notes50; } private set { notes50 = value; } }
+        public double Notes20 { get { return notes20; } private set { notes20 = value; } }
+        public double Notes10 { get { return notes10; } private set { notes10 = value; } }
+        public double Notes5 { get { return notes5; } private set { notes5 = value; } }
+        public double Coins2 { get { return coins2; } private set { coins2 = value; } }
+        public double Coins1 { get { return coins1; } private set { coins1 = value; } }
+        public double Coins50 { get { return coins50; } private set { coins50 = value; } }
+        public double Coins20 { get { return coins20; } private set { coins20 = value; } }
+        public double Coins10 { get { return coins10; } private set { coins10 = value; } }
+        public double Coins5 { get { return coins5; } private set { coins5 = value; } }
+        public double CoinsBronze { get { return coinsBronze; } private set { coinsBronze = value; } }
+        public double ChequeTotal { get { return chequeTotal; } private set { chequeTotal = value; } }
+        public double VisaTotal { get { return visaTotal; } private set { visaTotal = value; } }
+        public double CashChequeTotal { get { return cashChequeTotal; } private set { cashChequeTotal = value; } }
+        public double Discount { get { return discount; } private set { discount = value; } }
     }
 }
