6743943670b4477b406a2d91c7bbed8c69174d37
Alex <alexbass666@gmail.com>
2016-11-18 15:24:33 +0000

    Add batch summary table

--- Cheques/Default.aspx.cs
+++ Cheques/Default.aspx.cs
@@ -26,6 +26,12 @@ namespace Cheques
 
             ChqBatch[] cba = bcr.ChequeBatch(bcr.Results);
             int count = 1;
+            double sumCount = 0;
+            double sumTotal = 0;
+            DataTable sumDt = new DataTable();
+            sumDt.Columns.Add("Batch", typeof(int));
+            sumDt.Columns.Add("Number", typeof(double));
+            sumDt.Columns.Add("Total", typeof(double));
             foreach (ChqBatch cb in cba)
             {
                 DataTable dt = cb.Table;
@@ -51,8 +57,37 @@ namespace Cheques
                 gv.FooterRow.Cells[2].Text = String.Format("<strong>{0}</strong>", cb.TotalValue.ToString("C2"));
                 col.Controls.Add(gv);
 
+                sumDt.Rows.Add(count, cb.TotalAmount, cb.TotalValue);
+                sumCount += cb.TotalAmount;
+                sumTotal += cb.TotalValue;
+
                 count++;
             }
+            SummaryTable(sumDt, count, sumCount, sumTotal);
+        }
+
+        private void SummaryTable(DataTable _sumTable, int _count, double _sumCount, double _sumTotal)
+        {
+            HtmlGenericControl row = new HtmlGenericControl("div");
+            row.Attributes["class"] = "row";
+            batchTables.Controls.Add(row);
+
+            HtmlGenericControl col = new HtmlGenericControl("div");
+            col.Attributes["class"] = "col-xs-12";
+            row.Controls.Add(col);
+
+            col.Controls.Add(new LiteralControl(String.Format("<h2>CHEQUE BATCH LIST</h2>")));
+
+            GridView gv = new GridView();
+            gv.GridLines = GridLines.None;
+            gv.ShowFooter = true;
+            gv.CssClass = "table table-striped";
+            gv.DataSource = _sumTable;
+            gv.DataBind();
+            gv.FooterRow.Cells[0].Text = "<strong>TOTAL</strong>";
+            gv.FooterRow.Cells[1].Text = String.Format("<strong>{0}</strong>", _sumCount);
+            gv.FooterRow.Cells[2].Text = String.Format("<strong>{0}</strong>", _sumTotal.ToString("C2"));
+            col.Controls.Add(gv);
         }
 
         private void Gv_RowDataBound(object sender, GridViewRowEventArgs e)
